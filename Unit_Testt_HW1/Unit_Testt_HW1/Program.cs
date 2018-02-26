69d5d6cd28885435ad9ecd6c212535acbbfa6d56
Pooya448 <34579393+Pooya448@users.noreply.github.com>
2018-02-25 21:42:18 -0800

    Unit test completed using windows

---
+++ Unit_Testt_HW1/Unit_Testt_HW1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Testt_HW1
+{
+   
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                
+            }
+            public static bool IsSorted(int[] grades, bool asc)
+            {
+                int LastGrade = grades[0];
+                foreach (int grade in grades)
+                {
+                    if (asc)
+                    {
+                        if (grade < LastGrade)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            LastGrade = grade;
+                        }
+                    }
+
+                    else
+                    {
+                        if (grade > LastGrade)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            LastGrade = grade;
+                        }
+                    }
+                }
+                return true;
+
+            }
+
+        }
+    
+}

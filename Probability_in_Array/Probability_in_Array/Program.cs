3c207d2fcb6f6fd40c9ebc0d8e50fd585d65e637
Pooya Kabiri <pooya_kabiri@yahoo.com>
2018-02-27 12:43:48 -0800

    HW2

---
+++ Probability_in_Array/Probability_in_Array/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probability_in_Array
+{
+    public class Program
+    {
+        /// <summary>
+        /// calculating the probability of the selected integer by user using the probability 
+        /// principals and math operator : division
+        /// </summary>
+        /// <param name="num_array"> the main array which hosts the selected integer</param>
+        /// <param name="input"> the selected integer by the user for calculating it's probability
+        /// of being chosen by a random selection </param>
+        /// <returns></returns>
+        public static double calculate_prob (int[] num_array, int input)
+        {
+            /* declaration and initiation of t as integer used for counting the quantity 
+            of the selected integer in num_array */
+            double t = 0;
+            foreach (int num in num_array)
+            {
+                if (num == input)
+                {
+                    t++;
+                }
+            }
+            // the final quantity of the probability calculation
+            double prob = t / 8;
+            // returning the final result to the main function
+            return prob;
+        }
+        /// <summary>
+        /// getting the main array of integers and the selected integer grom user and calculating
+        /// the probability of be chosen by a random selection using the calculate_prob function
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            // the main integer array with 8 elements
+            int[] array = new int[8];
+            for (int i = 0; i<8; i++)
+            {
+                // getting the array elements from user as string and changing it into integer value
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            // holder of the selected integer by user
+            int input;
+            do
+            {
+                // getting integer from user as string value and changing it into int value
+                input = int.Parse(Console.ReadLine());
+                // printing the final result into the console
+                Console.WriteLine(calculate_prob(array,input));
+
+            } while (input != -1);
+            
+
+        }
+    }
+}
+
+
---
+++ Probability_in_Array/Probability_in_ArrayTests/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Probability_in_Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probability_in_Array.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void calculate_probTest()
+        {
+            // 3 test arrays for unit test assessment
+            int[] test1 = new int[] { 1, 3, 7, 3, 7, 9, 10, 5 };
+            int[] test2 = new int[] { 2, 7, 3, 8, 3, 9, 5, 8 };
+            int[] test3 = new int[] { 3, 8, 3, 6, 23, 3, 2, 7 };
+            // testing the three reliable cases using Assert function and AreEqual attribute
+            Assert.AreEqual(0.25, Program.calculate_prob(test1, 3), 0);
+            Assert.AreEqual(0.125, Program.calculate_prob(test2, 2), 0);
+            Assert.AreEqual(0.375, Program.calculate_prob(test3, 3), 0);
+        }
+    }
+}

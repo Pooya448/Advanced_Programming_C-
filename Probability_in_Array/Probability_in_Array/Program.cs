519770a8f4b3571eef20f1d354fda639839dea88
Pooya Kabiri <pooya_kabiri@yahoo.com>
2018-03-02 02:31:49 -0800

    Resolving Comments

--- Probability_in_Array/Probability_in_Array/Program.cs
+++ Probability_in_Array/Probability_in_Array/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Probability_in_Array
 {
@@ -12,16 +13,16 @@ namespace Probability_in_Array
         /// calculating the probability of the selected integer by user using the probability 
         /// principals and math operator : division
         /// </summary>
-        /// <param name="num_array"> the main array which hosts the selected integer</param>
+        /// <param name="array"> the main array which hosts the selected integer</param>
         /// <param name="input"> the selected integer by the user for calculating it's probability
         /// of being chosen by a random selection </param>
-        /// <returns> returns the probability result </returns>
-        public static double calculate_prob (int[] num_array, int input)
+        /// <returns> returns the probability result as double value </returns>
+        public static double calculate_prob(int[] array, int input)
         {
             /* declaration and initiation of counter as integer used for counting the quantity 
-            of the selected integer in num_array */
+            of the selected integer in array */
             double counter = 0;
-            foreach (int num in num_array)
+            foreach (int num in array)
             {
                 if (num == input)
                 {
@@ -34,6 +35,52 @@ namespace Probability_in_Array
             return prob;
         }
         /// <summary>
+        /// getting the selected integer from user ans returning the integer value
+        /// </summary>
+        /// <param name="selected_int"> a text stream which a console output or input stream
+        /// can be passed to </param>
+        /// <returns> returns ineteger value Parsed from a string value taken from the 
+        /// text stream "selected_int" </returns>
+        public static int input_selected(TextReader selected_int)
+        {
+            // getting integer from user as string value and changing it into int value
+            return int.Parse(selected_int.ReadLine());
+        }
+        /// <summary>
+        /// getting the main array from user (using console or a manual text stream) and calling 
+        /// input_selected function fro every element in the array
+        /// </summary>
+        /// <param name="array"> the main array of integers' allocated space in the memory passed
+        /// to the function by reference type of passing </param>
+        /// <param name="reader"> manual text stream having the default value of null </param>
+        /// <param name="starting_pos"> the starting position of for loop for unit testing 
+        /// having the default value of 0 when called in the main function </param>
+        public static void input_arr(ref int[] array, TextReader reader = null, int starting_pos = 0)
+        {
+
+            for (int i = starting_pos; i < 8; i++)
+            {
+                /* getting the array element of index i from user by calling input_selected function
+                   and changing it to int value at the same time */
+                array[i] = input_selected(reader);
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// writing the result output in a textwriter stream : System Console in the main program run calling and
+        /// a manual textwriter stream when unit testing
+        /// </summary>
+        /// <param name="result"> double value of final result of calculation </param>
+        /// <param name="writer"> a Textwriter stream of System.IO </param>
+        public static void output_result(double result, TextWriter writer)
+        {
+            // printing the final result into TextWriter stream of choice (Console by program running)
+            writer.WriteLine(result);
+        }
+        /// <summary>
         /// getting the main array of integers and the selected integer grom user and calculating
         /// the probability of be chosen by a random selection using the calculate_prob function
         /// </summary>
@@ -42,23 +89,21 @@ namespace Probability_in_Array
         {
             // the main integer array with 8 elements
             int[] array = new int[8];
-            for (int i = 0; i<8; i++)
-            {
-                // getting the array elements from user as string and changing it into integer value
-                array[i] = int.Parse(Console.ReadLine());
-            }
+            // getting the integer array from user by calling input_arr function
+            input_arr(ref array, Console.In);
             // holder of the selected integer by user
-            int input;
+            int input = 0;
             do
             {
-                // getting integer from user as string value and changing it into int value
-                input = int.Parse(Console.ReadLine());
-                // printing the final result into the console
-                Console.WriteLine(calculate_prob(array,input));
-
+                // getting the selected integer from the user by calling the input_selected function
+                input = input_selected(Console.In);
+                // breaking from the while loop if the user entered "-1" value.
+                if (input == -1)
+                    break;
+                /* writing the result to console using output_result function call and passing the
+                   calculate_prob function return value as the argument */
+                output_result(calculate_prob(array, input), Console.Out);
             } while (input != -1);
-            
-
         }
     }
 }
--- Probability_in_Array/Probability_in_ArrayTests/ProgramTests.cs
+++ Probability_in_Array/Probability_in_ArrayTests/ProgramTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Probability_in_Array.Tests
 {
@@ -19,9 +20,71 @@ namespace Probability_in_Array.Tests
             int[] test2 = new int[] { 2, 7, 3, 8, 3, 9, 5, 8 };
             int[] test3 = new int[] { 3, 8, 3, 6, 23, 3, 2, 7 };
             // testing the three reliable cases using Assert function and AreEqual attribute
-            Assert.AreEqual(0.25, Program.calculate_prob(test1, 3), 0);
-            Assert.AreEqual(0.125, Program.calculate_prob(test2, 2), 0);
-            Assert.AreEqual(0.375, Program.calculate_prob(test3, 3), 0);
+            Assert.AreEqual(0.25, Program.calculate_prob(test1, 3));
+            Assert.AreEqual(0.125, Program.calculate_prob(test2, 2));
+            Assert.AreEqual(0.375, Program.calculate_prob(test3, 3));
+        }
+        public void input_selectedTest()
+        {
+            // 3 const int values for unit testing
+            const int test_int1 = 8;
+            const int test_int2 = 5;
+            const int test_int3 = 2;    
+            // creating a StringReader and assigning a string value to it
+            using (StringReader test_reader = new StringReader(test_int1.ToString()))
+            {
+                // setting the console input stream to the manual StringReader
+                Console.SetIn(test_reader);
+                // comparing the function return value and expected value
+                Assert.AreEqual(Program.input_selected(test_reader), 8);
+            }
+            using (StringReader test_reader = new StringReader(test_int2.ToString()))
+            {
+                Console.SetIn(test_reader);
+                Assert.AreEqual(Program.input_selected(test_reader), 5);
+            }
+            using (StringReader test_reader = new StringReader(test_int3.ToString()))
+            {
+                Console.SetIn(test_reader);
+                Assert.AreEqual(Program.input_selected(test_reader), 2);
+            }
+        }
+        public void input_arrTest()
+        {
+            // 2 arrays for testing
+            int[] test1 = new int[8];
+            int[] arr_test1 = new int[] { 1, 3, 7, 3, 7, 3, 5, 8 };
+            for (int i = 0; i < 8; i++)
+            {
+                // creating a StringReader and assigning a string value to it
+                using (StringReader test_reader = new StringReader(arr_test1[i].ToString()))
+                {
+                    Program.input_arr(ref test1, test_reader,i);
+                }
+            }
+            // counting the number of elements of 2 arrays which should be equal one by one
+            // counter counts the number of equality between 2 test arrays' elements
+            int counter = 0;
+            for (int j = 0; j < 8; j++)
+                if (test1[j] == arr_test1[j])
+                    counter++;
+            // comparing counter and the number 8 : 
+            Assert.AreEqual(counter, 8);
+            
+        }
+        public void output_resultTest()
+        {
+            // test double value for testing
+            const double test = 12.87;
+            // creating a StringWriter 
+            using (StringWriter test_writer = new StringWriter())
+            {
+                Program.output_result(test, test_writer);
+                /* getting the written value by output_result function from the test_writer 
+                   StringWriter and comparing it to the expected string value */
+                string output_test = test_writer.GetStringBuilder().ToString();
+                Assert.AreEqual(output_test, test.ToString());
+            }
         }
     }
 }

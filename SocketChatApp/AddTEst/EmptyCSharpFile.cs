10c8969acdc6193c8c9a09113403393bd411394b
Pooya Kabiri <pooya_kabiri@yahoo.com>
2018-06-18 20:08:13 +0430

    test1

---
+++ SocketChatApp/AddTEst/EmptyCSharpFile.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SocketChatApp {
+    [TestClass]
+    public class AddTests{
+        [TestMethod]
+        public void AddtestMethod(){
+            Assert.AreEqual(5, Program.Add(2, 3));
+        }
+    }
+}
---
+++ SocketChatApp/SocketChatApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketChatApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int Add (int a,int b){
+            return a + b;
+        }
+    }
+}
